6851f073ef077c8e3a5d56331191544e094e37b5
Simon Hryszko <simon@hryszko.dev>
2021-08-26 14:43:44 +0200

    Auto backup!

--- Scripts/cleaner.sh
+++ Scripts/cleaner.sh
@@ -1,53 +1,85 @@
 #! /bin/bash
-# set -x
+#set -x
 export DISPLAY=:0.0
 
-# true - delete, else - move
-function process(){
-	if [ "$2" = "DELETE" ]; then
-		rm -r "$1" && notify-send "$3 $1 deleted!"
-	else
-    mv "$1" "../.Downloads/$1" && notify-send "$3 $1 moved to ~/.Downloads/$1!"
-	fi
-}
+# Arguments: 
+# $1: mode (delete or move)
+# $2: path to the folder to clean
+# $3: number of days to delete/move
 
-# remove empty directories
-for emptyDir in `find ~/Desktop/ ~/Downloads/ -empty -type d`; do
-	notify-send "Directoy $emptyDir was deleted, because was empty!"
-done
-find ~/Desktop/ ~/Downloads/ -empty -type d -delete
-
-# remove / move old files / directorie
-if [ -z "$1" ] || [ "${1,,}" != "delete" ] && [ "${1,,}" != "move" ]; then
-	echo "Give as parameter mode ( delete / move )"
-	exit
+# Chceck arguments
+if [ $# -ne 3 ]; then
+	notify-send "Usage: $0 <mode> <path> <days>"
+	exit 1
 fi
 
-if [ "${1,,}" == "delete" ]; then
-	day=30
-	path=".Downloads"
-	mode="DELETE"
-else
-	day=2
-	path="Downloads"
-	mode="MOVE"
-  cr  
-  if [ ! -d "~/.Downloads" ]; then
-    mkdir -p ~/.Downloads
-  fi
+# Check mode
+if [ "$1" != "delete" ] && [ "$1" != "move" ]; then
+	notify-send "Mode must be delete or move"
+	exit 1
 fi
 
-cd ~/$path
+# Check path
+if [ ! -d "$HOME/$2" ]; then
+	notify-send "Path does not exist in your home directory"
+	exit 1
+fi
 
-for element in *
-do
-	if [ -d "$element" ]; then
-		if [ $(find "$element" -type f -mtime -$day | wc -l) -eq 0 ]; then
-			process "$element" "$mode" "Directory"
+# Check days
+if [ "$3" -lt 1 ] || [ "$3" -gt 365 ]; then
+	notify-send "Days must be between 1 and 365"
+	exit 1
+fi
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# Go to the folder
+cd $HOME/$2
+
+# Make variable count
+count=0
+
+# loop through files
+for file in *; do
+	# check if file is a directory
+	if [ -d "$file" ]; then
+		# check if folder is older than days - 1
+		if [ ! "$(find "$(printf '%q' "$file")" -type f -mtime -$(($3 - 1)))" ]; then
+			# delete/mode folder
+			if [ "$1" == "delete" ]; then
+				rm -rf "$file"
+			else
+				mv "$file" "$HOME/.$2"
+			fi
+			# increment count
+			count=$((count+1))
+			echo "$file"
 		fi
 	else
-		if [ $(find `pwd` -name "$element" -mtime +$day | wc -l) -gt 0 ]; then
-			process "$element" "$mode" "File"
+		# check if file is older than days
+		if [ "$(find . -name "$(printf '%q' "$file")" -type f -mtime +$(($3 - 1)))" ]; then
+			# delete/mode file
+			if [ "$1" == "delete" ]; then
+				rm "$file"
+			else
+				mv "$file" "$HOME/.$2"
+			fi
+			# increment count
+			count=$((count+1))
+			echo "$file"
 		fi
 	fi
 done
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# notify if counte is grater than 0
+if [ "$count" -gt 0 ]; then
+	# if mode is move
+	if [ "$1" == "move" ]; then
+		notify-send "`basename "$0"`" "Moved $count files and/or directories"
+	else
+		# notify
+		notify-send "`basename "$0"`" "Deleted $count files and/or directories" -u critical
+	fi
+fi
