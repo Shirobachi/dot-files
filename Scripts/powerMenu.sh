ffd0b845aab6e8c009ba3e238c0306c3f7438a4a
Simon Hryszko <simon@hryszko.dev>
2021-09-11 15:35:22 +0200

    Auto backup!

--- Scripts/powerMenu.sh
+++ Scripts/powerMenu.sh
@@ -11,18 +11,64 @@ yes=${yes[$randomChoice]}
 no=${no[$randomChoice]}
 sure=${sure[$randomChoice]}
 
-if [ "$1" == "restart" ] && [ "`echo -e \"$yes\n$no\" | rofi -dmenu -p \"$sure\" -width 10 -lines 2`" == "$yes" ]; then
+# Function to save layout before exit
+function saveLayout() {
+	# for 0..9
+	for i in {0..9}; do
+		# save layout
+		i3-resurrect save -w $i -d $HOME/.i3
+
+		# check if file $HOME/.i3/workspace_${i}_programs.json has less or equal than 2b and check if file $HOME/.i3/workspace_${i}_layout.json has less or equal than 2b
+		if [ $(wc -c < $HOME/.i3/workspace_${i}_programs.json) -le 2 ] && [ $(wc -c < $HOME/.i3/workspace_${i}_layout.json) -le 2 ]; then
+			# remove file $HOME/.i3/workspace_${i}_programs.json and $HOME/.i3/workspace_${i}_layout.json
+			rm $HOME/.i3/workspace_${i}_programs.json 2>/dev/null
+			rm $HOME/.i3/workspace_${i}_layout.json 2>/dev/null
+		fi
+	done
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Flags
+# if one parameter is passed and it is "--restart" or "-r"
+if [ $# -eq 1 ] && [ "$1" == "--restart" ] || [ "$1" == "-r" ] && [ "`echo -e \"$yes\n$no\" | rofi -dmenu -p \"$sure\" -width 10 -lines 2`" == "$yes" ]; then
+	# save layout
+	basename "$0" -s
+
 	notify-send "Restarting..."
 	killall chrome
 	sleep 1 && systemctl reboot
-elif [ "$1" == "shutdown" ] && [ "`echo -e \"$yes\n$no\" | rofi -dmenu -p \"$sure\" -width 10 -lines 2`" == "$yes" ]; then
+
+# if one parameter is passed and it is "--poweroff" or "-p"
+elif [ $# -eq 1 ] && [ "$1" == "--poweroff" ] || [ "$1" == "-p" ] && [ "`echo -e \"$yes\n$no\" | rofi -dmenu -p \"$sure\" -width 10 $(basename2 "$0")`" == "$yes" ]; then
+	# save layout
+	basename "$0" -s
+
 	notify-send "Shutdowning..."
 	killall chrome
 	sleep 1 && systemctl poweroff
-elif [ "$1" == "logout" ] && [ "`echo -e \"$yes\n$no\" | rofi -dmenu -p \"$sure\" -width 10 -lines 2`" == "$yes" ]; then
+
+# if one parameter is passed and it is "--exit" or "-e"
+elif [ $# -eq 1 ] && [ "$1" == "--exit" ] || [ "$1" == "-e" ] && [ "`echo -e \"$yes\n$no\" | rofi -dmenu -p \"$sure\" -width 10 $(basename2 "$0")`" == "$yes" ]; then
+	# save layout
+	basename "$0" -s
+
 	notify-send "Logout..."
 	killall chrome
 	sleep 1 && i3-msg exit
-elif [ "$1" == "lock" ]; then
+
+# if one parameter is passed and it is "--lock" or "-l"
+elif [ $# -eq 1 ] && [ "$1" == "--lock" ] || [ "$1" == "-l" ]; then
 	i3lock-fancy -p
+
+# if one parameter is passed and it is "--save-layout" or "-s"
+elif [ $# -eq 1 ] && [ "$1" == "--save-layout" ] || [ "$1" == "-s" ]; then
+	# run saveLayout()
+	saveLayout && exit 0 || notify-send -u critical "`basename "$0"`" "Could not save layout!"
+
+else
+	echo Wrong parameter!
+	echo "Usage: `basename "$0"` [--restart|-r] [--poweroff|-p] [--exit|-e] [--lock|-l] [--save-layout|-s]"
+	exit 1
+
 fi
