550a38c804983b9ce30965f8244e0b854f2062cf
Simon Hryszko <simon@hryszko.dev>
2021-09-11 22:41:46 +0200

    Auto backup!

--- Scripts/powerMenu.sh
+++ Scripts/powerMenu.sh
@@ -11,7 +11,7 @@ yes=${yes[$randomChoice]}
 no=${no[$randomChoice]}
 sure=${sure[$randomChoice]}
 
-# Function to save layout before exit
+# Function to save layout 
 function saveLayout() {
 	# for 0..9
 	for i in {0..9}; do
@@ -27,22 +27,34 @@ function saveLayout() {
 	done
 }
 
+# Function to restore layout
+function restoreLayout() {
+	# for 0..9
+	for i in {0..9}; do
+		# check if file $HOME/.i3/workspace_${i}_programs.json and $HOME/.i3/workspace_${i}_layout.json exists
+		if [ -f $HOME/.i3/workspace_${i}_programs.json ] && [ -f $HOME/.i3/workspace_${i}_layout.json ]; then
+			# restore layout
+			i3-resurrect restore -w $i -d $HOME/.i3
+		fi
+	done
+}
+
 # ----------------------------------------------------------------------------------------------------------------------
 
 # Flags
 # if one parameter is passed and it is "--restart" or "-r"
 if [ $# -eq 1 ] && [ "$1" == "--restart" ] || [ "$1" == "-r" ] && [ "`echo -e \"$yes\n$no\" | rofi -dmenu -p \"$sure\" -width 10 -lines 2`" == "$yes" ]; then
 	# save layout
-	notify-send	dirname "$0"/basename "$0" -sl
+	`dirname "$0"`/`basename "$0"` -sl|| notify-send basename "$0" "Error during saving layout"
 
 	notify-send "Restarting..."
-	# killall chrome
-	# sleep 1 && systemctl reboot
+	killall chrome
+	sleep 1 && systemctl reboot
 
 # if one parameter is passed and it is "--poweroff" or "-p"
 elif [ $# -eq 1 ] && [ "$1" == "--poweroff" ] || [ "$1" == "-p" ] && [ "`echo -e \"$yes\n$no\" | rofi -dmenu -p \"$sure\" -width 10 -lines 2 $(basename2 "$0")`" == "$yes" ]; then
 	# save layout
-	basename "$0" -sl
+	`dirname "$0"`/`basename "$0"` -sl|| notify-send basename "$0" "Error during saving layout"
 
 	notify-send "Shutdowning..."
 	killall chrome
@@ -51,7 +63,7 @@ elif [ $# -eq 1 ] && [ "$1" == "--poweroff" ] || [ "$1" == "-p" ] && [ "`echo -e
 # if one parameter is passed and it is "--exit" or "-e"
 elif [ $# -eq 1 ] && [ "$1" == "--exit" ] || [ "$1" == "-e" ] && [ "`echo -e \"$yes\n$no\" | rofi -dmenu -p \"$sure\" -width 10 -lines 2 $(basename2 "$0")`" == "$yes" ]; then
 	# save layout
-	basename "$0" -sl
+	`dirname "$0"`/`basename "$0"` -sl|| notify-send basename "$0" "Error during saving layout"
 
 	notify-send "Logout..."
 	killall chrome
@@ -62,15 +74,18 @@ elif [ $# -eq 1 ] && [ "$1" == "--lock" ] || [ "$1" == "-l" ]; then
 	i3lock-fancy -p
 
 # if one parameter is passed and it is "--save-layout" or "-sl"
-elif [ $# -eq 1 ] && [ "$1" == "--save-layout" ] || [ "$1" == "-s" ]; then
+elif [ $# -eq 1 ] && [ "$1" == "--save-layout" ] || [ "$1" == "-sl" ]; then
 	# run saveLayout()
 	saveLayout && exit 0 || notify-send -u critical "`basename "$0"`" "Could not save layout!"
 
 # if one parameter is passed and it is "--restart-layout" or "-rl"
+elif [ $# -eq 1 ] && [ "$1" == "--restart-layout" ] || [ "$1" == "-rl" ]; then
+	# run restoreLayout()
+	restoreLayout && exit 0 || notify-send -u critical "`basename "$0"`" "Could not restore layout!"
 
+# else or -h or --help
 else
 	echo Wrong parameter!
 	echo "Usage: `basename "$0"` [--restart|-r] [--poweroff|-p] [--exit|-e] [--lock|-l] [--save-layout|-s]"
 	exit 1
-
 fi
