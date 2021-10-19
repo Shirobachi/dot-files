20f22cd04db8935700ec42f88d525406d354ebff
Simon Hryszko <simon@hryszko.dev>
2021-10-19 16:00:01 +0200

    Auto backup!

--- Scripts/my-scripts/screenshot-maker.sh
+++ Scripts/my-scripts/screenshot-maker.sh
@@ -3,11 +3,16 @@
 # hide notifications
 killall dunst
 
-flameshot full -c
 window=$(xdotool getactivewindow getwindowname | grep -ci "teams\|skype")
 
 # if $windows is 1, then the window is teams
 if [ $window -eq 1 ]; then
+
+	xdotool mousemove 0 0
+	xdotool click 1
+	sleep .4
+	flameshot full -c
+
 	# Go to 1 workspace
 	i3-msg workspace 9 &&
 
@@ -20,4 +25,6 @@ if [ $window -eq 1 ]; then
 
 	# Go previous workspace
 	i3-msg workspace back_and_forth || notify-send "Error" "Error occupied!"
+else
+	flameshot full -c
 fi
