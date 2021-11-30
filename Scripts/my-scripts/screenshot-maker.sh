c13f121b96597ae4f233032e7fe26279cf9f9478
Simon Hryszko <simon@hryszko.dev>
2021-11-30 16:00:01 +0100

    Auto backup!

--- Scripts/my-scripts/screenshot-maker.sh
+++ Scripts/my-scripts/screenshot-maker.sh
@@ -23,9 +23,11 @@ if [ $window -eq 1 ]; then
 
 	# confirm return
 	xdotool key Return &&
+	sleep 0.5 &&
 
 	# Go previous workspace
 	i3-msg workspace back_and_forth || notify-send "Error" "Error occupied!"
 else
 	flameshot full -c
 fi
+
