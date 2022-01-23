9721d4bf1e2085246526341ec285b615e49d25e7
Simon Hryszko <simon@hryszko.dev>
2022-01-23 18:00:01 +0100

    Auto backup!

--- Scripts/my-scripts/restart.sh
+++ Scripts/my-scripts/restart.sh
@@ -4,19 +4,20 @@ export network=`echo $(ip route | grep '^default' | awk '{print $5}' | head -n1)
 export temperature=`for i in /sys/class/hwmon/hwmon*/temp*_input; do echo "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*})) $i"; done | tail -2 | head -1 | cut -d " " -f 4`
 
 # set screen setup
+xrandr | grep -i "Disconnected" | cut -d " " -f1 | xargs -I{} xrandr --output {} --off
+xrandr | grep -i " connected" | cut -d " " -f1 | xargs -I{} xrandr --output {} --auto
 if [ $(xrandr | grep " connected" | wc -l) -gt 1 ]; then
 	#xrandr --output DP-0 --auto && xrandr --output "DP-2" --auto --output "DP-0" --left-of "DP-2"
-	xrandr --output DP-2 --auto && 
 	xrandr --output DP-0 --pos 0x0 --rotate left --output DP-2 --pos 1080x560
 	notify-send "External monitor detected!"
 else
-	xrandr | grep -i "Disconnected" | cut -d " " -f1 | xargs -I{} xrandr --output {} --off
 	notify-send "No external monitor detected!"
 fi
 
 # kill notification daemon
 killall -q dunst
 
+sleep 1
 # polybar
 if killall polybar; sleep 1 && polybar -q asus -c ~/.config/polybar/config.ini; sleep .5 && polybar -q main -c ~/.config/polybar/config.ini || [ "$1" = "--exit" ]; then
 		# exit if parameter is set
