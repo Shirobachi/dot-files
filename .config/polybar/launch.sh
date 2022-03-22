30a10013682461a09d10fe1cd601943628bc5470
Simon Hryszko <simon@hryszko.dev>
2022-03-22 23:00:01 +0100

    Auto backup!

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -11,4 +11,5 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+#polybar -q main -c "$DIR"/config.ini &
+xrandr | grep " connected" | cut -d" " -f1 | xargs -I{} cat config.ini | bash -c "sed 's/screenID/{}/' > /tmp/polybarTempConfig && polybar -q main -c /tmp/polybarTempConfig"
