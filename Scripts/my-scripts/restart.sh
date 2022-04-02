e0c12d19ef4600a44c73c3975a6862d00154f35e
Simon Hryszko <simon@hryszko.dev>
2022-04-02 14:00:01 +0200

    Auto backup!

--- Scripts/my-scripts/restart.sh
+++ Scripts/my-scripts/restart.sh
@@ -6,7 +6,7 @@ export temperature=`for i in /sys/class/hwmon/hwmon*/temp*_input; do echo "$(<$(
 xrandr | grep -i "Disconnected" | cut -d " " -f1 | xargs -I{} xrandr --output {} --off
 xrandr | grep -i " connected" | cut -d " " -f1 | xargs -I{} xrandr --output {} --auto
 if [ $(xrandr | grep " connected" | wc -l) -gt 1 ]; then
-	xrandr --output "DP-0" --auto --above "DP-2"
+	xrandr --output "DP-0" --auto --above "eDP1"
 fi
 
 # restart services and new wallpaper
