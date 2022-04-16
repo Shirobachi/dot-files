fc14c38e2f6e98f2663da2db98825f3f4980dadc
Simon Hryszko <simon@hryszko.dev>
2022-04-16 16:22:12 +0200

    Auto backup!

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -12,7 +12,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1 | tac); do
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1 | cat); do
     MONITOR=$m polybar -c "$DIR/config.ini" --reload main &
   done
 else
