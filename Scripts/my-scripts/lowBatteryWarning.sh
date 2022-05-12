4ca4c92a97c2fd74005494e1dfff22ba97c9d10c
Simon Hryszko <simon@hryszko.dev>
2022-05-13 00:46:34 +0200

    Auto backup!

--- Scripts/my-scripts/lowBatteryWarning.sh
+++ Scripts/my-scripts/lowBatteryWarning.sh
@@ -3,9 +3,11 @@
 
 BATTINFO=$(acpi -b)
 if echo "$BATTINFO" | grep -q "Discharging"; then
-#	if [[ $(echo "$BATTINFO" | cut -f 5 -d " ") < 00:05:00 ]]; then
-#		notify-send -u critical "Battery empty, shutting down" "Your layout will be attempted to save"; $HOME/Scripts/my-scripts/sessionManager.sh -s; systemctl poweroff
 	if [[ $(echo "$BATTINFO" | cut -f 5 -d " ") < 00:30:00 ]]; then
 			DISPLAY=:0.0 /usr/bin/notify-send "Low battery" "$BATTINFO"
-    fi
+  fi
+	if [[ $(echo "$BATTINFO" | cut -f 5 -d " ") < 00:05:00 ]]; then
+    /home/simon/Scripts/my-scripts/sessionManager.sh -s && poweroff 
+  fi
+
 fi
